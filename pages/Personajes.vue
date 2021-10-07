83545f2c1162195acc3bbb31a88646224675271a
pole <zleckzxd@gmail.com>
2021-10-07 17:28:18 -0300

    added character tab + removed unnecessary nav items

---
+++ pages/Personajes.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-row>
+      <v-col align=center>
+        <h1>Personajes</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="character in characters" :key="character.id" xl="4">
+        <v-card class="rounded-xl">
+          <v-img class="mx-auto" :src="character.imagen" width="200px"/>
+          <v-card-title class="justify-center">{{ character.nombre }}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    characters() {
+      return this.$store.state.characterModule.characters
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <v-row>
+      <v-col align=center>
+        <h1>Bienvenido</h1>
+      </v-col>
+    </v-row>
+  </div>
+</template>
